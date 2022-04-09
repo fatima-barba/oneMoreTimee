76df21063ad0b33510a914b2493c3a8d29d9606e
fatima <fatimaa@vandenrobotics.com>
2022-04-09 00:14:29 -0700

    java files addeed

--- app/src/main/java/com/example/onemoretime/MainActivity.java
+++ app/src/main/java/com/example/onemoretime/MainActivity.java
@@ -1,23 +1,179 @@
 package com.example.onemoretime;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.Context.MODE_PRIVATE;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.onemoretime.data.repo.CompetitionsRepo;
-import com.example.onemoretime.dialogs.DialogListener;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity implements DialogListener {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    private final String TAG = MainActivity.class.getSimpleName();
+import com.example.onemoretime.R;
+import com.example.onemoretime.databinding.ActivityMainBinding;
 
-    private CompetitionsRepo competitionsRepo;
+import java.util.ArrayList;
 
+public class MainActivity extends Fragment {
+
+    private Spinner teamSpinner;
+    private FragmentMatchHomeBinding binding;
+    private int match = 1;
+    ArrayList<Integer> teams = new ArrayList<>();
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentMatchHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        addTeams();
+        setUpSpinners();
+
+        Button button = root.findViewById(R.id.button_start_match_scouting);
+        Button allianceSelector = root.findViewById(R.id.matchHomeAllianceSelector);
+        Button plusMatch = root.findViewById(R.id.plusMatch);
+        Button minusMatch = root.findViewById(R.id.minusMatch);
+        TextView matchNum = root.findViewById(R.id.textView_matchNumber);
+        matchNum.setText(String.valueOf(match));
+
+        SharedPreferences preferences = getActivity().getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        if(!preferences.contains("deviceAlliance")) {
+            editor.putString("deviceAlliance", "red");
+        }
+        if(preferences.getString("deviceAlliance", "").equals("red")) {
+            allianceSelector.setText("Red");
+            allianceSelector.setBackgroundColor(Color.parseColor("#FF6961"));
+        }
+
+        if(preferences.getString("deviceAlliance", "").equals("blue")) {
+            allianceSelector.setText("Blue");
+            allianceSelector.setBackgroundColor(Color.parseColor("#6495ED"));
+        }
+        editor.apply();
+        plusMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                match++;
+                matchNum.setText(String.valueOf(match));
+            }
+        });
+
+        minusMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (match > 1) {
+                    match--;
+                    matchNum.setText(String.valueOf(match));
+                }
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_match_auto);
+                SharedPreferences preferences = getActivity().getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("matchScouting", match);
+                editor.putInt("teamScouting", Integer.parseInt(teamSpinner.getSelectedItem().toString()));
+                editor.apply();
+            }
+        });
+        allianceSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getActivity().getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                if(preferences.getString("deviceAlliance", "").equals("blue")) {
+                    allianceSelector.setText("Red");
+                    allianceSelector.setBackgroundColor(Color.parseColor("#FF6961"));
+                    editor.putString("deviceAlliance", "red");
+                }
+                if(preferences.getString("deviceAlliance", "").equals("red")) {
+                    allianceSelector.setText("Blue");
+                    allianceSelector.setBackgroundColor(Color.parseColor("#6495ED"));
+                    editor.putString("deviceAlliance", "blue");
+                }
+                editor.apply();
+            }
+        });
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+                BottomNavigationView scoutingBar = root.findViewById(R.id.scouting_bar);
+                NavigationUI.setupWithNavController(scoutingBar, navController);
+            }
+        }, 10);
+
+        return root;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    public void addTeams(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference("Events/UCD/teams");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot rawSnapshot) {
+                teams.clear();
+                for (DataSnapshot snapshot : rawSnapshot.getChildren()) {
+                    if (snapshot.getKey() != null) {
+                        teams.add(Integer.valueOf(snapshot.getKey()));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    public void setUpSpinners() {
+        View root = binding.getRoot();
+        teams.add(0);
+        teamSpinner = root.findViewById(R.id.teamSpinner);
+        ArrayAdapter<Integer> teamAdapter = new ArrayAdapter<>(root.getContext(), android.R.layout.simple_spinner_dropdown_item, teams);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(teamAdapter);
+
+        teamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice = parent.getItemAtPosition(position).toString();
+                Toast.makeText(binding.getRoot().getContext(), choice, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 }
-
--- app/src/main/java/com/example/onemoretime/MatchSouting/MatchAutoFragment.java
+++ app/src/main/java/com/example/onemoretime/MatchSouting/MatchAutoFragment.java
@@ -1,4 +1,130 @@
 package com.example.onemoretime.MatchSouting;
 
-public class MatchAutoFragment {
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import org.robovikes.frost.R;
+import org.robovikes.frost.Utils.SavePage;
+import org.robovikes.frost.databinding.FragmentMatchAutoBinding;
+
+public class MatchAutoFragment extends Fragment{
+
+    private FragmentMatchAutoBinding binding;
+    protected FragmentActivity Activity;
+    public int totalAutoScoreL ;
+    public int totalAutoScoreR;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof Activity){
+            Activity = (FragmentActivity) context;
+        }
+    }
+    public MAuto() {
+    }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        binding = FragmentMatchAutoBinding.inflate(inflater, container, false);
+        ViewGroup root = binding.getRoot();
+        Button autoPlusL = root.findViewById(R.id.autoPlusL);
+        Button autoMinusL = root.findViewById(R.id.autoMinusL);
+        Button autoPlusR = root.findViewById(R.id.autoPlusR);
+        Button autoMinusR = root.findViewById(R.id.autoMinusR);
+        Button teleNav = root.findViewById(R.id.auto_bar);
+        TextView autoScoreL = root.findViewById(R.id.textView_upperScoreAuto);
+        TextView autoScoreR = root.findViewById(R.id.textView_lowerScoreAuto);
+        TextView teamAuto = root.findViewById(R.id.textView_teamAuto);
+        TextView matchAuto = root.findViewById(R.id.textView_matchAuto);
+        TextView allianceAuto = root.findViewById(R.id.textView_allianceAuto);
+        autoScoreL.setText(String.valueOf(totalAutoScoreL));
+        autoScoreR.setText(String.valueOf(totalAutoScoreR));
+        SavePage.loadSave(this, root);
+        SharedPreferences preferences = Activity.getPreferences(MODE_PRIVATE);
+        teamAuto.setText("Team: " + preferences.getInt("teamScouting", -1));
+        matchAuto.setText("Match: " + preferences.getInt("matchScouting", -1));
+        allianceAuto.setText("Alliance: " + preferences.getString("deviceAlliance", ""));
+        totalAutoScoreR = preferences.getInt("AutoLowerScore", -1);
+        totalAutoScoreL = preferences.getInt("AutoUpperScore", -1);
+        autoScoreL.setText(String.valueOf(totalAutoScoreL));
+        autoScoreR.setText(String.valueOf(totalAutoScoreR));
+
+        autoPlusL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                totalAutoScoreL++;
+                autoScoreL.setText(String.valueOf(totalAutoScoreL));
+            }
+        });
+        autoMinusL.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(totalAutoScoreL > 0){
+                    totalAutoScoreL--;
+                    autoScoreL.setText(String.valueOf(totalAutoScoreL));
+                }
+            }
+        }));
+
+        autoPlusR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                totalAutoScoreR++;
+                autoScoreR.setText(String.valueOf(totalAutoScoreR));
+            }
+        });
+
+        autoMinusR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(totalAutoScoreR > 0){
+                    totalAutoScoreR--;
+                    autoScoreR.setText(String.valueOf(totalAutoScoreR));
+                }
+            }
+        });
+        teleNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(Activity, R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_match_tele);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        ViewGroup root = binding.getRoot();
+        Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        SharedPreferences preferences = Activity.getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("AutoUpperScore", totalAutoScoreL);
+        editor.putInt("AutoLowerScore", totalAutoScoreR);
+        editor.apply();
+        SavePage.saveLayout(this, root);
+        binding = null;
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        binding = null;
+    }
 }
