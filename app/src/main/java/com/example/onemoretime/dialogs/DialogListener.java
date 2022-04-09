c5ffe8fea2d1d12c69f3d0f851ed4d10027e52da
Chichi <Chichii@vandenrobotics.com>
2022-04-08 23:33:25 -0700

    Deleted stuff

--- app/src/main/java/com/example/onemoretime/dialogs/DialogListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.onemoretime.dialogs;
-
-public interface DialogListener {
-    public void onDialogPositiveClick(android.support.v4.app.DialogFragment dialog);
-    public void onDialogNegativeClick(android.support.v4.app.DialogFragment dialog);
-
-}
-
